39dc20e2efa807ae8c6a5da8d280953d23533440
Inzemam Khan <inzemamkhan@MacBook-Air.local>
2026-02-21 20:33:11 +0530

    fix: Splitting raw_text for tokenizing

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 url = ("https://raw.githubusercontent.com/rasbt/"
        "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
        "the-verdict.txt")
@@ -6,5 +7,6 @@ file_path = "the-verdict.txt"
 urllib.request.urlretrieve(url, file_path)
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+result = re.split(r'([,.:;?_!"()\']|--|\\s)', raw_text)
+result = [item.strip() for item in result if item.strip()]
+print(result)
